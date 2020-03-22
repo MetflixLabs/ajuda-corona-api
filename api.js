373bd4d2e09ea4e33d820a7bc2793658fc2f593d
pt-br <ltavares@yieldstreet.com>
2020-03-22 16:46:39 -0300

    Prettier

--- api.js
+++ api.js
@@ -1,31 +1,31 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+import axios from 'axios';
 
-import axios from 'axios'
+dotenv.config();
 
-const BASE_URL = process.env.BASE_URL
-const SITE_KEY = process.env.SITE_KEY
-const X_API_ID = process.env.X_API_ID
-const X_API_KEY = process.env.X_API_KEY
+const BASE_URL = process.env.BASE_URL;
+const SITE_KEY = process.env.SITE_KEY;
+const X_API_ID = process.env.X_API_ID;
+const X_API_KEY = process.env.X_API_KEY;
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     'X-API-ID': X_API_ID,
-    'X-API-KEY': X_API_KEY
+    'X-API-KEY': X_API_KEY,
   },
-})
+});
 
 const params = {
   'site-key': SITE_KEY,
-  currency: 'mintme'
-}
+  currency: 'mintme',
+};
 
 export const get = async (endpoint = '') => {
   try {
-    const res = await api.get(endpoint, { params })
-    return res.data
+    const res = await api.get(endpoint, { params });
+    return res.data;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
